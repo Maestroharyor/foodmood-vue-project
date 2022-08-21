b97988b66117e29855f4f112691d40e7a63ffae1
maestroharyor <Haryor1josh@gmail.com>
2022-08-21 18:05:41 +0100

    Added search functionality

--- src/components/headers/Header.vue
+++ src/components/headers/Header.vue
@@ -20,7 +20,7 @@
           class="font-[400] text-lg hover:text-green-700 transition duration-300 ease-in-out">Contact</router-link>
       </div>
 
-      <div>
+      <div class="relative">
         <label class="relative block">
           <span class="sr-only">Search</span>
           <span class="absolute inset-y-0 left-0 flex items-center pl-3 pr-8">
@@ -31,17 +31,67 @@
             class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-12 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
             placeholder="Search for recipes..."
             type="text"
-            name="search" />
+            name="search"
+            v-model="searchTerm"
+            @keyup="searchForItem()" />
         </label>
+
+
+        <div class=" px-5 pt-2 pb-6 absolute top-[40px] left-0 bg-white rounded-b shadow-md w-[230px] z-[5]"
+          v-if="loading || searchResults.length">
+          <div v-if="loading"
+            class="max-w-[500px] px-5 mx-auto flex items-center justify-center">
+            <font-awesome-icon icon="fa-solid fa-circle-notch"
+              class="text-green-600 text-4xl animate-spin py-5" />
+          </div>
+          <div class="flex flex-col gap-5 divide-y-2"
+            v-if="searchResults.length && !loading">
+            <router-link
+              :to="{ name: 'recipesDetails', params: { id: `${item.strMeal.toLowerCase().split(' ').join('-')}-${item.idMeal}` } }"
+              class="pt-3"
+              v-for="(item, index) in searchResults"
+              :key="index">
+              <p>{{ item.strMeal }}</p>
+            </router-link>
+
+          </div>
+
+        </div>
       </div>
     </nav>
   </header>
 </template>
 
 <script lang="ts">
+import { mealsDBDataType } from "@/data/dataTypes";
 import { defineComponent } from "vue";
+import axios from "axios";
+
 export default defineComponent({
   name: "HeaderComponent",
+  data() {
+    return {
+      searchTerm: "",
+      loading: false,
+      searchResults: [] as mealsDBDataType[],
+    }
+  },
+  methods: {
+    searchForItem() {
+      if (this.searchTerm.length && !this.loading) {
+        this.loading = true
+        axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${this.searchTerm}`)
+          .then(response => {
+            this.loading = false
+            this.searchResults = response.data.meals.slice(0, 5)
+          })
+          .catch(error => {
+            this.loading = false
+            console.log(error)
+          })
+      }
+    }
+  }
 });
 </script>
 <style lang=""></style>
